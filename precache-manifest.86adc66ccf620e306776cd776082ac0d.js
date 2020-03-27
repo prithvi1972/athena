412746fb9d0cf4b00473d1c2a33601f3afca75e5
Prithvi <46812606+prithvi1974@users.noreply.github.com>
2020-03-27 09:40:19 +0530

    Updates

---
+++ precache-manifest.86adc66ccf620e306776cd776082ac0d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92e233e3027627cd4e84fb2a4eb67f37",
+    "url": "/athena/index.html"
+  },
+  {
+    "revision": "b51d3fc18b64dbf990a2",
+    "url": "/athena/static/css/main.53e0e2e3.chunk.css"
+  },
+  {
+    "revision": "2c01c40f6d12081dc4fa",
+    "url": "/athena/static/js/2.de457c1b.chunk.js"
+  },
+  {
+    "revision": "2c598234c7a24970b5fdf032f602e7bc",
+    "url": "/athena/static/js/2.de457c1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b51d3fc18b64dbf990a2",
+    "url": "/athena/static/js/main.24c5464e.chunk.js"
+  },
+  {
+    "revision": "3b57e6d5d796ef9a58bf",
+    "url": "/athena/static/js/runtime-main.4febf3d4.js"
+  }
+]);
