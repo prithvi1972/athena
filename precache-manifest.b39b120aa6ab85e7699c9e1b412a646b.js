6eb7aa45f4a7f224257e43ed1fc4e614a100bdcc
Prithvi <46812606+prithvi1974@users.noreply.github.com>
2020-03-28 16:49:47 +0530

    Updates

---
+++ precache-manifest.b39b120aa6ab85e7699c9e1b412a646b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88fac6f714f50d0e835ae6f31836b865",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b75bdcf5282341fbab05",
+    "url": "/static/css/main.ca7b0b65.chunk.css"
+  },
+  {
+    "revision": "62b2791d17e391a0f008",
+    "url": "/static/js/2.7389acdb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.7389acdb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b75bdcf5282341fbab05",
+    "url": "/static/js/main.17115468.chunk.js"
+  },
+  {
+    "revision": "955ec54781367b3a929c",
+    "url": "/static/js/runtime-main.89f29052.js"
+  },
+  {
+    "revision": "2480ae73ad8985a192a4fd1c132e60ec",
+    "url": "/static/media/facebook.2480ae73.svg"
+  },
+  {
+    "revision": "80819046a463448a7d16a06f7cd5b39b",
+    "url": "/static/media/linkedin.80819046.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/athena/precache-manifest.4e72beaa8272f35040a2752da5683ead.js"
+  "/precache-manifest.b39b120aa6ab85e7699c9e1b412a646b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/athena/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
